89d28b854315c1662f14f6b3df03e785b112b9d6
Om4roFF <52379357+Om4roFF@users.noreply.github.com>
2019-12-01 19:41:39 +0600

    Model and test-drive.html
    Добавил модель в главную стр и изменил тест-драйв

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    mark = models.CharField(max_length=200)
+    model = models.CharField(max_length=200)
+    body = models.CharField(max_length=200)
+    drive_unit = models.CharField(max_length=200)
+    engine_volume = models.FloatField(max_length=10)
+
+    def __str__(self):
+        return self.model
+
+
